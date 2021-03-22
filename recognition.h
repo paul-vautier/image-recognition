b13d7f32cd9fa1b3432943444a2eea35c6aa9173
L. Thomassin <lucasthomassin61@gmail.com>
2021-03-22 21:45:55 +0100

    commentaires

--- recognition.h
+++ recognition.h
@@ -68,14 +68,29 @@ Path* compute_path(PGMData* data);
 bool verify_image(PGMData* data,Path* model,bool with_reporting);
 
 /**
- * 
+ * this function crops all the pixels since the coordonates given as parameters 
+ * which are the coordonates of the top left corner of where you're cropping, 
+ * then it determinates the theoric other borders of the box following the ring model dimensions
+ * @param data your PGM data field
+ * @param x the beginning x
+ * @param y the beginning y
+ * @return the box obtained
  */
 BoundingBox get_bounding_box(PGMData* data, int x,int y);
 
 /**
- * 
+ * get the min y of the given ring
+ * @param data all the pixels as PGMData
+ * @return the vector2 of the min y
  */
 Vector2 get_min_y_position(PGMData* data,int px, int py);
+
+/**
+ * get the min x of the given ring
+ * @param data all the pixels as PGMData
+ * @return the vector2 of the min x
+ */
+Vector2 get_min_x_position(PGMData* data,int px, int py)
 
 /**
  * 
@@ -83,17 +98,24 @@ Vector2 get_min_y_position(PGMData* data,int px, int py);
 Path* init_path(PGMData* data, int* i, int *j);
 
 /**
- * 
+ * @param box the boudingbox you're working on
+ * @param x x-coordonate of the point 
+ * @param y y-coordonate of the point
+ * @return the int value of the bool explaining if the given point with x andy is in the box
  */
 int in(BoundingBox box, int x, int y);
 
 /**
- * 
+ * get the max y of the given ring
+ * @param data all the pixels as PGMData
+ * @return the int of the max x
  */
 int get_max_y_position(PGMData* data, int px, int py);
 
 /**
- * 
+ * get the min y of the given ring
+ * @param data all the pixels as PGMData
+ * @return the int of the max x
  */
 int get_max_x_position(PGMData* data, int px, int py);
 
